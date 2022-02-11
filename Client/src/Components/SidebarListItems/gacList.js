503a75bb558554a1de68286a4d3ecf745ad04cea
Naveed-Ahmed-Khan <naveedahmedkhan12234@gmail.com>
2022-02-11 16:05:30 +0500

    GAc List Update

--- Client/src/Components/SidebarListItems/gacList.js
+++ Client/src/Components/SidebarListItems/gacList.js
@@ -33,6 +33,7 @@ import BarChartIcon from "@mui/icons-material/BarChart";
 import LayersIcon from "@mui/icons-material/Layers";
 import AssignmentIcon from "@mui/icons-material/Assignment";
 import HomeIcon from "@mui/icons-material/Home";
+import ListIcon from "@mui/icons-material/List";
 
 export const gacListitems = [
   {
@@ -107,33 +108,50 @@ export const gacListitems = [
     path: "/Dashboard/AddSupervisoryCommittee",
   },
  */
+
   {
-    text: "Evaluate Synopsis (MS)",
-    icon: <AnalyticsIcon />,
-    path: "/Dashboard/EvaluateSynopsis(MS)",
+    text: "Evaluate Synopsis",
+    icon: <ListIcon />,
+    active: false,
+    subMenu: [
+      {
+        text: "Synopsis MS",
+        icon: <AnalyticsIcon />,
+        path: "/Dashboard/EvaluateSynopsis(MS)",
+      },
+
+      {
+        text: " Synopsis PhD",
+        icon: <MapIcon />,
+        path: "/Dashboard/EvaluateSynopsis(PhD)",
+      },
+    ],
   },
 
   {
-    text: "Evaluate Synopsis (PhD)",
-    icon: <MapIcon />,
-    path: "/Dashboard/EvaluateSynopsis(PhD)",
-  },
-  {
-    text: "Pending Thesis",
-    icon: <EditNotificationsIcon />,
-    path: "/Dashboard/PendingThesis",
-  },
-  {
-    text: "Evaluate Thesis (MS)",
-    icon: <AnalyticsIcon />,
-    path: "/Dashboard/EvaluateThesis(MS)",
+    text: "Evaluate Thesis",
+    icon: <ListIcon />,
+    active: false,
+    subMenu: [
+      {
+        text: "Pending Thesis",
+        icon: <EditNotificationsIcon />,
+        path: "/Dashboard/PendingThesis",
+      },
+      {
+        text: "Thesis MS",
+        icon: <AnalyticsIcon />,
+        path: "/Dashboard/EvaluateThesis(MS)",
+      },
+
+      {
+        text: "Thesis PhD",
+        icon: <MapIcon />,
+        path: "/Dashboard/EvaluateThesis(PhD)",
+      },
+    ],
   },
 
-  {
-    text: "Evaluate Thesis (PhD)",
-    icon: <MapIcon />,
-    path: "/Dashboard/EvaluateThesis(PhD)",
-  },
   /* {
     text: "Manage Notification",
     icon: <EditNotificationsIcon />,
@@ -158,51 +176,66 @@ export const gacListitems = [
     path: "/Dashboard/SendNotificationtoAll",
   },
  */
+
   {
-    text: "View MS Student Details",
-    icon: <PageviewIcon />,
-    path: "/Dashboard/ViewMSStudentDetails",
+    text: "Student Details",
+    icon: <ListIcon />,
+    active: false,
+    subMenu: [
+      {
+        text: "MS Students",
+        icon: <PageviewIcon />,
+        path: "/Dashboard/ViewMSStudentDetails",
+      },
+
+      {
+        text: "PhD Students",
+        icon: <ContentPasteSearchIcon />,
+        path: "/Dashboard/ViewPhDStudentDetails",
+      },
+    ],
   },
 
   {
-    text: "View PhD Student Details",
-    icon: <ContentPasteSearchIcon />,
-    path: "/Dashboard/ViewPhDStudentDetails",
-  },
+    text: "Reports",
+    icon: <ListIcon />,
+    active: false,
+    subMenu: [
+      {
+        text: "Supervisor's Report",
+        icon: <PeopleIcon />,
+        path: "/Dashboard/Supervisor'sReport",
+      },
+      {
+        text: "Program-Wise Report",
+        icon: <LeaderboardIcon />,
+        path: "/Dashboard/Program-WiseReport",
+      },
 
-  {
-    text: "Supervisor's Report",
-    icon: <PeopleIcon />,
-    path: "/Dashboard/Supervisor'sReport",
-  },
-  {
-    text: "Program-Wise Report",
-    icon: <LeaderboardIcon />,
-    path: "/Dashboard/Program-WiseReport",
-  },
+      {
+        text: "Thesis-Wise Report",
+        icon: <TableChartIcon />,
+        path: "/Dashboard/Thesis-WiseReport",
+      },
 
-  {
-    text: "Thesis-Wise Report",
-    icon: <TableChartIcon />,
-    path: "/Dashboard/Thesis-WiseReport",
-  },
+      {
+        text: "Synopsis-Wise Report",
+        icon: <PieChartIcon />,
+        path: "/Dashboard/Synopsis-WiseReport",
+      },
 
-  {
-    text: "Synopsis-Wise Report",
-    icon: <PieChartIcon />,
-    path: "/Dashboard/Synopsis-WiseReport",
-  },
+      {
+        text: "Supervisor/Program-Wise Report",
+        icon: <FindInPageIcon />,
+        path: "/Dashboard/ViewSupervisorProgram-WiseReport",
+      },
 
-  {
-    text: "View Supervisor/Program-Wise Report",
-    icon: <FindInPageIcon />,
-    path: "/Dashboard/ViewSupervisorProgram-WiseReport",
-  },
-
-  {
-    text: "Session-Wise Report",
-    icon: <ScreenSearchDesktopIcon />,
-    path: "/Dashboard/Session-WiseReport",
+      {
+        text: "Session-Wise Report",
+        icon: <ScreenSearchDesktopIcon />,
+        path: "/Dashboard/Session-WiseReport",
+      },
+    ],
   },
 
   {
